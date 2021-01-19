ae0c5eca6a26dad9eafbe225b74e98d18ad23c71
sayb <romsanbryan@gmail.com>
2021-01-19 20:31:11 +0100

    Add method to capitalice first letter

---
+++ src/main/java/lib/romsanbryan/utils/StringUtils.java
@@ -0,0 +1,32 @@
+package lib.romsanbryan.utils;
+
+public class StringUtils {
+
+	public static String capitalice(String str) {
+		str = lowerString(str);
+		StringBuilder sb = new StringBuilder(str.length());
+		sb.append(str.substring(0, 1).toUpperCase()).append(str.substring(1));
+		return sb.toString();
+	}
+	
+	public static String capitaliceAll(String str) {
+		str = lowerString(str);
+		StringBuilder sb = new StringBuilder(str.length());
+		String[] words = str.split(" ");
+		for (String word : words) {
+			if (word.length() > 1)
+				sb.append(word.substring(0, 1).toUpperCase()).append(word.substring(1));
+			else
+				sb.append(word.toUpperCase());
+
+			sb.append(" ");
+		}
+		return sb.toString().trim();
+	}
+	
+	public static String lowerString(String str) {
+		if (str == null)
+			return null;
+		return str.toLowerCase();
+	}
+}
